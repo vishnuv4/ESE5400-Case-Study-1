b6dba03ee2d0472218c140d825db6e5eec8c3682
Vishnu Venkatesh <vishnuv@seas.upenn.edu>
2024-10-15 01:41:20 -0400

    added scripts

---
+++ calc.py
@@ -0,0 +1,28 @@
+def tax_rate():
+    tax = {}
+    tax["2004"] = 7414/17910
+    tax["2005"] = 12830/32723
+    tax["2006"] = 11747/30447
+    for i in tax.keys():
+        print(f"Tax rate in year {i}: {tax[i]}")
+
+    avg_tax_rate = (tax["2004"] + tax["2005"] + tax["2006"]) / 3
+    print(f"Average tax rate: {avg_tax_rate}")
+    return avg_tax_rate
+
+def wacc(t):
+    l = 0.422
+    rd = 0.0628
+    re = 0.1091
+    wacc = (l * (1 - t) * rd) + ((1 - l) * re)
+    print(f"WACC: {wacc}")
+
+def asset_beta():
+    l = 0.422
+    b_equity = 1.25
+    b_asset = (1 - l) * b_equity
+    print(f"Asset beta: {b_asset}")
+
+if __name__ == "__main__":
+    t = tax_rate()
+    wacc(t)
